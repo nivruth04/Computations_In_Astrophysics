f90c240acd4773e8c80ca66cef73c72bcdbcfbda
Nivruth Narayan  V M <nivruth0408@gmail.com>
2026-06-10 18:59:20 +0530

    Update title to include Euler's time step

--- freefall/rk4_vs_eulers.m
+++ freefall/rk4_vs_eulers.m
@@ -1,7 +1,5 @@
 run('freefall_RK4.m')
 run('freefall_eulers.m')
-
-
 figure;
 plot(t_rk4, r_rk4, 'b', 'DisplayName', 'RK4')
 hold on
@@ -9,4 +7,4 @@ plot(t_eulers, r_eulers, 'r--', 'DisplayName', 'Euler')
 legend
 xlabel('Time (s)')
 ylabel('Distance (m)')
-title('RK4 vs Euler — Free Fall Comparison')
+title("RK4 vs Euler's(dt="+dt_eulers+")— Free Fall Comparison")
